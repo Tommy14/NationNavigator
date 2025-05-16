d0134055b28b051e9390c2248589ba01ac6e52e7
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-05-16 15:53:00 +0530

    Add Jest configuration for setting up test environment and transforming files with Babel presets

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFiles: ['<rootDir>/jest-setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTest.js'],  
+    moduleNameMapper: {
+    '^services/api$': '<rootDir>/__mocks__/services/api.js',
+    '^services/auth$': '<rootDir>/__mocks__/services/auth.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^.+\\.(png|jpg|jpeg|svg|gif)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
+    },
+    transformIgnorePatterns: [
+      '/node_modules/(?!(react-globe.gl|react-kapsule|topojson-client|jerrypick|d3-.*|globe\.gl)/)'
+    ],
+  };
