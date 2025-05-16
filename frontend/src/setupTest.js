4544dc3b0e03b830a4de4141b1a17112e584ce22
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-05-16 15:53:15 +0530

    Add Jest environment setup with testing library and global environment variables

---
+++ frontend/src/setupTest.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+
+// Environment variables
+global.import = {
+  meta: {
+    env: {
+      VITE_API_URL: "http://localhost:9000",
+      MODE: "test",
+      DEV: true,
+    },
+  },
+};
